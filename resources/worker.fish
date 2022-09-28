8d3bbc37b1995e0a6d083a0e35e6c7bf2974c825
esdmr <esdmr0@gmail.com>
2022-09-29 00:14:06 +0330

    Fix worker script

--- resources/worker.fish
+++ resources/worker.fish
@@ -10,29 +10,28 @@ function _vscode_complete
     commandline (string unescape --style url -- "$argv")
     echo current (commandline -t) >&9
 
-    for line in (complete -C)
-        set content (string match -r '^.+?(?=\\t|$)' -- $line)
+    for line in (complete -C --escape)
+        set -l content (string match -r '^.+?(?=\\t|$)' -- $line)
+        set -l type Text
 
         switch "$content"
             case if for while break continue function return begin end and or not switch case
-                printf 'complete %s\t' Keyword >&9
+                set type Keyword
             case '$*'
-                printf 'complete %s\t' Variable >&9
+                set type Variable
             case '*'
-                set -l normalized (eval echo\ (string escape --style url -- $content) | string unescape --style url)
+                set -l normalized (eval echo\ --\ (string escape --style url -- $content) | string unescape --style url)
 
                 if test -f "$normalized"
-                    printf 'complete %s\t' File >&9
+                    set type File
                 else if test -d "$normalized"
-                    printf 'complete %s\t' Folder >&9
-                else if type -q "$content"
-                    printf 'complete %s\t' Function >&9
-                else
-                    printf 'complete %s\t' Text >&9
+                    set type Folder
+                else if type -q -- "$content"
+                    set type Function
                 end
         end
 
-        echo $line >&9
+        printf 'complete %s\t%s\n' "$type" "$line" >&9
     end
 
     exit 0
