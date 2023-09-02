d7daf0faad03c6d6ffa9b6a40e44d5f3bd1e85f3
esdmr <esdmr0@gmail.com>
2023-09-02 13:54:33 +0330

    Update worker command to depend on platform

--- src/fish/worker.ts
+++ src/fish/worker.ts
@@ -1,6 +1,7 @@
 import {mkdtempSync, rmSync} from 'node:fs';
 import {tmpdir} from 'node:os';
 import {join} from 'node:path';
+import {platform} from 'node:process';
 import {createInterface} from 'node:readline';
 import type {Readable} from 'node:stream';
 import {fileURLToPath} from 'node:url';
@@ -29,61 +30,92 @@ export function startWorker(options: {
 		options.fishPath = 'fish';
 	}
 
-	const child = execa(
-		'script',
-		[
-			'-e',
-			'-q',
-			'-c',
-			`${options.fishPath} -iPC 'source $_FISH_COMPLETION_WORKER'`,
-			'/dev/null',
+	const command = getCommand(options.fishPath);
+	const child = execa(command[0], command.slice(1), {
+		stdio: [
+			'pipe', // 0 stdin
+			'pipe', // 1 stdout
+			'pipe', // 2 stderr
+			'ignore', // 3 unused
+			'ignore', // 4 unused
+			'ignore', // 5 unused
+			'ignore', // 6 unused
+			'ignore', // 7 unused
+			'ignore', // 8 unused
+			'pipe', // 9 output
 		],
-		{
-			stdio: [
-				'pipe', // 0 stdin
-				'pipe', // 1 stdout
-				'pipe', // 2 stderr
-				'ignore', // 3 unused
-				'ignore', // 4 unused
-				'ignore', // 5 unused
-				'ignore', // 6 unused
-				'ignore', // 7 unused
-				'ignore', // 8 unused
-				'pipe', // 9 output
-			],
-			cwd: options.cwd,
-			env: {
-				/* eslint-disable @typescript-eslint/naming-convention */
-				TERM: 'dumb',
-				GIO_LAUNCHED_DESKTOP_FILE: undefined,
-				VISUAL: undefined,
-				CHROME_DESKTOP: undefined,
-				VSCODE_GIT_ASKPASS_NODE: undefined,
-				VSCODE_GIT_ASKPASS_MAIN: undefined,
-				TERM_PROGRAM: undefined,
-				GIT_ASKPASS: undefined,
-				VSCODE_GIT_IPC_HANDLE: undefined,
-				EDITOR: undefined,
-				VSCODE_GIT_ASKPASS_EXTRA_ARGS: undefined,
-				_FISH_COMPLETION_ASSIST: options.isAssistantEnabled
-					? 'v1'
-					: 'disabled',
-				_FISH_COMPLETION_TEMP_DIR: temporaryDir,
-				_FISH_COMPLETION_WORKER: fileURLToPath(
-					new URL('worker.fish', import.meta.url),
-				),
-				/* eslint-enable @typescript-eslint/naming-convention */
-			},
-			timeout: options.timeout ?? 10_000,
-			signal: options.signal!,
+		cwd: options.cwd,
+		env: {
+			/* eslint-disable @typescript-eslint/naming-convention */
+			TERM: 'dumb',
+			GIO_LAUNCHED_DESKTOP_FILE: undefined,
+			VISUAL: undefined,
+			CHROME_DESKTOP: undefined,
+			VSCODE_GIT_ASKPASS_NODE: undefined,
+			VSCODE_GIT_ASKPASS_MAIN: undefined,
+			TERM_PROGRAM: undefined,
+			GIT_ASKPASS: undefined,
+			VSCODE_GIT_IPC_HANDLE: undefined,
+			EDITOR: undefined,
+			VSCODE_GIT_ASKPASS_EXTRA_ARGS: undefined,
+			_FISH_COMPLETION_ASSIST: options.isAssistantEnabled
+				? 'v1'
+				: 'disabled',
+			_FISH_COMPLETION_TEMP_DIR: temporaryDir,
+			_FISH_COMPLETION_WORKER: fileURLToPath(
+				new URL('worker.fish', import.meta.url),
+			),
+			/* eslint-enable @typescript-eslint/naming-convention */
 		},
-	);
+		timeout: options.timeout ?? 10_000,
+		signal: options.signal!,
+	});
 
 	return {
 		child,
 		inputChannel: child.stdin!,
 		outputChannel: (child.stdio as Readable[])[9]!,
 	};
+}
+
+function getCommand(fishPath: string): [string, ...string[]] {
+	switch (platform) {
+		case 'android':
+		case 'linux':
+		case 'netbsd': {
+			return [
+				'script',
+				'-e',
+				'-q',
+				'-c',
+				`${fishPath} -iPC 'source $_FISH_COMPLETION_WORKER'`,
+				'/dev/null',
+			];
+		}
+
+		case 'darwin':
+		case 'freebsd': {
+			return [
+				'script',
+				'-q',
+				'/dev/null',
+				fishPath,
+				'-iPC',
+				'source $_FISH_COMPLETION_WORKER',
+			];
+		}
+
+		case 'aix':
+		case 'openbsd':
+		case 'sunos':
+		case 'win32': {
+			throw new Error(`Unsupported platform: ${platform}`);
+		}
+
+		default: {
+			throw new Error(`Unknown platform: ${platform}`);
+		}
+	}
 }
 
 export function debugStdOutputAndError(
