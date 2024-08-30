b33bba6c3e4477d152a119cc93bd734bef6c218f
esdmr <esdmr0@gmail.com>
2024-08-30 15:46:29 +0330

    Implement deployment in CI

--- scripts/package.js
+++ scripts/package.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import process from 'node:process';
 import {execa as execa_} from 'execa';
 
 const execa = execa_({
@@ -8,7 +9,7 @@ const execa = execa_({
 await execa`node scripts/prepack.js`;
 
 try {
-	await execa`pnpm exec vsce package --no-dependencies`;
+	await execa`pnpm exec vsce package --no-dependencies ${process.argv.slice(2)}`;
 } finally {
 	await execa`node scripts/postpack.js`;
 }
